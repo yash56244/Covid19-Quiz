fa14ac64fa35071a77367c554707b167fa8afd77
yashvc738 <yash56244@gmail.com>
2020-08-23 15:07:16 +0530

    completed user name input and storing scores in local storage

--- script.js
+++ script.js
@@ -112,7 +112,8 @@ var quizDuration = 0;
 var quizSecondElapsed = 0;
 var quizInterval;
 var currentQuestion = 0;
-// var highScore = document.getElementById("highScore");
+var username;
+var highScore = document.getElementById("highScore");
 var quizTimer = document.getElementById("quizTimer");
 var quiz = document.getElementById("quiz");
 var timerTable = document.getElementById("timer");
@@ -129,11 +130,20 @@ function init() {
     instructions.setAttribute("id", "instructions")
     instructions.textContent = "If you answer correctly you will score points.";
     quiz.appendChild(instructions);
+    let nameLabel = document.createElement("label");
+    nameLabel.setAttribute("for", "name");
+    nameLabel.textContent = "Enter Name : ";
+    let nameInput = document.createElement("input");
+    nameInput.setAttribute("id", "name");
+    nameInput.setAttribute("name", "name");
+    quiz.appendChild(nameLabel);
+    quiz.appendChild(nameInput);
     let startQuiz = document.createElement("button");
     startQuiz.setAttribute("id", "startQuiz");
     startQuiz.textContent = "Start Quiz!";
     quiz.appendChild(startQuiz);
     startQuiz.addEventListener("click", function(){
+        username = nameInput.value;
         startquiz(questions);
     })
 }
@@ -285,6 +295,24 @@ function endQuiz(){
     quiz.appendChild(heading);
     quiz.appendChild(instructions);
     quiz.appendChild(playAgain);
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+    var d = new Date(),
+        h = (d.getHours()<10?'0':'') + d.getHours(),
+        m = (d.getMinutes()<10?'0':'') + d.getMinutes();
+    let t = h + ':' + m;
+    let thisScore = [{username : username, score : score, date : today, time : t}];
+    let storedScores = JSON.parse(localStorage.getItem("quizScores"));
+    if(storedScores == null){
+        storedScores = thisScore;
+    }else{
+        storedScores.push(thisScore[0]);
+    }
+    localStorage.setItem("quizScores", JSON.stringify(storedScores));
+    highScores();
     playAgain.addEventListener("click", init);
 }
 
@@ -293,4 +321,39 @@ function stopTimer() {
     clearInterval(quizInterval);
 }
 
+function highScores() {
+    stopTimer();
+    clear();
+    timerTable.setAttribute("style", "visibility: hidden");
+    let storedScores = JSON.parse(localStorage.getItem("quizScores"));
+    let heading = document.createElement("h2");
+    heading.setAttribute("id", "heading");
+    heading.textContent = "Top 5 High Scores";
+    quiz.appendChild(heading);
+    if(storedScores != null){
+        storedScores.sort((a, b) => (a.score < b.score) ? 1: -1);
+        let scoresToDisplay = 5;
+        if(storedScores.length < 5){
+            scoresToDisplay = storedScores.length;
+        }
+        for(let i = 0; i < scoresToDisplay; i++){
+            var s = storedScores[i];
+            var p = document.createElement("p");
+            p.textContent = s.score + " by " + s.username + " on " + s.date +  " at " + s.time;
+            quiz.appendChild(p);
+        }
+    }else{
+        let p = document.createElement("p");
+        p.textContent = "Your High Scores will be shown here";
+        quiz.append(p);
+    }
+    let playAgain = document.createElement("button");
+    playAgain.setAttribute("id", "startQuiz");
+    playAgain.textContent = "Play Quiz!";
+    quiz.appendChild(playAgain);
+    playAgain.addEventListener("click", init);
+}
+
 init();
+
+highScore.addEventListener("click", highScores);
